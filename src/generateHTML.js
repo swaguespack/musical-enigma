01accbffba9f79baea9656fac307addefa5db852
shellby.miller <shellby.miller@gmail.com>
2023-02-22 00:06:37 -0500

    updates to generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,66 +1,64 @@
+// Creates manager card
 const managerCard = (teamArray) => {
     
     return `<section>
-    <div class="container">
+    <div class="container is-fluid">
         <div class="manager">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <div class="card-header bg-primary text-white">
-                    <h4 class="card-title"><span class="material-icons">local_cafe</span>${teamArray.getName()}</h4>
-                    <h5 class="card-subtitle mb-2">Manager</h5>
+                <div class="card">
+                    <div "card-header has-background-primary has-text-white">
+                    <h4 class="card-header-title has-text-white">${teamArray.name}</h4>
+                    <h5 class="card-header-icon has-text-weight-semibold is-size-5"><span class="material-icons p-1">local_cafe</span>Manager</h5>
                     </div>
-                    <p></p>
-                    <h6 class="card-text">ID: ${teamArray.id}</h6>
-                    <h6 class="card-text">Email: <a href="mailto:${teamArray.email}">${teamArray.email}</a></h6>
-                    <h6 class="card-text">Office Number: ${teamArray.officeNumber}</h6>
-                </div>
-            </div>
+                    <div class="card-content">
+                    <h6 class="p-2 is-size-5">ID: ${teamArray.id}</h6>
+                    <h6 class="p-2 is-size-5">Email: <a href="mailto:${teamArray.email}">${teamArray.email}</a></h6>
+                    <h6 class="p-2 is-size-5">Office Number: ${teamArray.officeNumber}</h6>
+                    </div>
+                 </div>
         </div>
     </div>
     </section>`
     
     };
     
+// Creates engineer card
     const engineerCard = (teamArray) => {
     
         return `<section>
-            <div class="container">
+            <div class="container is-fluid">
                 <div class="engineer">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <div class="card-header bg-primary text-white">
-                            <h4 class="card-title"><span class="material-icons">engineering</span>${teamArray.name}</h4>
-                            <h5 class="card-subtitle mb-2">Engineer</h5>
+                    <div class="card">
+                            <div class="card-header has-background-primary has-text-white">
+                            <h4 class="card-header-title has-text-white">${teamArray.name}</h4>
+                            <h5 class="card-header-icon has-text-weight-semibold is-size-5"><span class="material-icons p-1">engineering</span>Engineer</h5>
                             </div>
-                            <p></p>
-                            <h6 class="card-text">ID: ${teamArray.id}</h6>
-                            <h6 class="card-text">Email: <a href="mailto:${teamArray.email}">${teamArray.email}</a></h6>
-                            <h6 class="card-text">GitHub: <a href="https://github.com/${teamArray.github}" target="_blank" rel="noopener noreferrer">${teamArray.github}</a></h6>
+                            <h6 class="p-2 is-size-5">ID: ${teamArray.id}</h6>
+                            <h6 class="p-2 is-size-5">Email: <a href="mailto:${teamArray.email}">${teamArray.email}</a></h6>
+                            <h6 class="p-2 is-size-5">GitHub: <a href="https://github.com/${teamArray.github}" target="_blank" rel="noopener noreferrer">${teamArray.github}</a></h6>
                         </div>
-                    </div>
                 </div>
             </div>
         </section>`;
         }
     
+    // Creates intern card
         const internCard = (teamArray) => {
         
             return `
         <section>
-            <div class="container">
+            <div class="container is-fluid">
                 <div class="intern">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <div class="card-header bg-primary text-white">
-                            <h4 class="card-title"><span class="material-icons">school</span>${teamArray.name}</h4>
-                            <h5 class="card-subtitle mb-2">Intern</h5>
+                        <div class="card">
+                        <div class="card-header has-background-primary has-text-white">
+                        <h4 class="card-header-title has-text-white">${teamArray.name}</h4>
+                        <h5 class="card-header-icon has-text-weight-semibold is-size-5"><span class="material-icons p-1">school</span>Intern</h5>
                             </div>
                             <p></p>
-                            <h6 class="card-text">ID: ${teamArray.id}</h6>
-                            <h6 class="card-text">Email: <a href="mailto:${teamArray.email}">${teamArray.email}</a></h6>
-                            <h6 class="card-text">School: ${teamArray.school}</h6>
+                            <h6 class="p-2 is-size-5">ID: ${teamArray.id}</h6>
+                            <h6 class="p-2 is-size-5">Email: <a href="mailto:${teamArray.email}">${teamArray.email}</a></h6>
+                            <h6 class="p-2 is-size-5">School: ${teamArray.school}</h6>
                         </div>
-                    </div>
+                    
                 </div>
             </div>
         </section>
@@ -84,18 +82,15 @@ const managerCard = (teamArray) => {
                 cardInput += engineerCard(teamArray[i]);
             }
         }
-        console.log(cardInput);
         return cardInput;
        
     }
-    
-    //export function to generate entire page
+
     const generateHTML = (teamArray) => {
-        //destructure page teamArraya by type of employee... 
+
         console.log(teamArray);
         
        
-    
         return `
         <!DOCTYPE html>
     <html lang="en">
@@ -105,16 +100,15 @@ const managerCard = (teamArray) => {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Team Portfolio</title>
         <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
     <body>
-        <header>
-            <h1>My Team</h1>
+        <header class="hero is-primary p-6 has-text-centered">
+            <h1 class="title">My Team</h1>
         </header>
-    <main>
-        <div class="flex-wrap">
+    <main class="hero-body">
+        <div class="is-flex is-justify-content-center">
     ${cards(teamArray)}
     </div>
     </main> 
